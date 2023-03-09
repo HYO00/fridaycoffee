6115a6a06347b430bc89606762551e224add82e4
HYO00 <79884004+HYO00@users.noreply.github.com>
2023-03-09 10:01:37 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+    let pronArr = ["aya", "ye", "woo", "ma"];
+    let copyBabbling = babbling;
+
+    for(let i =0; i < copyBabbling.length; i++){
+        for(let j=0; j < pronArr.length; j++){
+            copyBabbling[i] =copyBabbling[i].replaceAll(pronArr[j], '1')
+         }  
+
+        if(copyBabbling[i].includes('e')){
+          copyBabbling[i] = copyBabbling[i].replace('e', 'z')
+        }
+    }
+    
+    const result = copyBabbling.filter((el) => (!Number.isNaN(Number(el))))
+
+    return result.length;
+}
