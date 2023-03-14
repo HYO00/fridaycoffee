0973e2c9b3d5f12e354941dca8c7eff9cb507e21
HYO00 <79884004+HYO00@users.noreply.github.com>
2023-03-14 13:42:23 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,14 @@
+function solution(common) {
+    var answer =0;
+ 
+    const 등차 = common[common.length-1] - common[common.length-2]
+
+    const 등비 = common[1] / common[0]
+
+    if(common[1] - common[0] === 등차){
+        answer = common[common.length-1] + 등차
+    }else{
+        answer = common[common.length-1] * 등비
+    }
+    return answer;
+}
