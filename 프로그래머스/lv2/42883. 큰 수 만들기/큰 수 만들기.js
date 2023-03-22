063e83a3fb1fc56b87e5b5e5c158721287da23aa
HYO00 <79884004+HYO00@users.noreply.github.com>
2023-03-23 08:07:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,20 +1,18 @@
 function solution(number, k) {
     const numArr = [...number];
     const stack = [];
-    
-    for (let i = 0; i < numArr.length; i++) {
-        while (stack.length > 0 && stack[stack.length - 1] < numArr[i] && k > 0) {
+
+    for(let i = 0; i < numArr.length; i++){ 
+        
+        while(stack.length && stack[stack.length-1] < numArr[i] && k > 0){
             stack.pop();
-            k--;
-        }
-        stack.push(numArr[i]);
+            k= k-1;
+        }  
+        stack.push(numArr[i]); 
+        
     }
-    
-    while (k > 0) {
-        stack.pop();
-        k--;
-    }
-
-    return stack.join('');
+    return stack.join('').substr(0, number.length-k);
 }
-
+//while을 통해 stack에 숫자가 담겨있을 경우 반목문의 해당 요소보다 작으면 비워준다.
+//stack에서 뺐으니까 k도 줄여준다.
+//같은 숫자일 경우 모두 스택에 담겨있을거기 때문에 k수를 제외한 문자열을 리턴한다.
